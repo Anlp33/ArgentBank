b48ec8b493953911491920b02d5a3b02c405f1c8
Anne-laure POIRÉ <82209328+Anlp33@users.noreply.github.com>
2022-09-02 18:38:03 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles/main.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import User from "./pages/User";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/reducers/getUser.js
@@ -0,0 +1,16 @@
+const initialState = {
+  firstName: undefined,
+  lastName: undefined,
+};
+
+const getUserReducer = (state = initialState, action) => {
+  if (action.type === "GET_USER_DATA") {
+    return {
+      ...state,
+      firstName: action.payload.firstName,
+      lastName: action.payload.lastName,
+    };
+  }
+  return state;
+};
+export default getUserReducer;
